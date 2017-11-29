eb2330446c609fd8e79d60c86766f1cd479ed2eb
Rob Price <rob.price@libertymutual.com>
2017-11-29 15:02:46 -0500

    page title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My app';
 
   constructor(private router: Router) {}
 
